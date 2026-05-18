0d63b6bea4bbaa648f71062edc7ad07a47f1aa07
ashiqueappuz04 <ashiqueappuz04@gmail.com>
2026-05-18 15:15:38 +0530

    second commit

--- neww.py
+++ neww.py
@@ -6,3 +6,4 @@ def is_palindrome(s):
     cleaned = ''.join(c.lower() for c in s if c.isalnum())
     return cleaned == cleaned[::-1]
 #explain the code line by line:1. `def is_palindrome(s):` - This line defines a function named `is_pal
+print("hello")
